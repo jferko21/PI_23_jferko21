1f31dfbfeb40a9b5c31000405624df8a2f8e40f4
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-04-24 13:27:24 +0200

    Dodan window za prijavu

--- EvaluationManager/Form1.Designer.cs
+++ EvaluationManager/Form1.Designer.cs
@@ -23,20 +23,78 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.lblKorIme = new System.Windows.Forms.Label();
+            this.lblLozinka = new System.Windows.Forms.Label();
+            this.txtKorIme = new System.Windows.Forms.TextBox();
+            this.txtLozinka = new System.Windows.Forms.TextBox();
+            this.btnPrijava = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblKorIme
+            // 
+            this.lblKorIme.AutoSize = true;
+            this.lblKorIme.Location = new System.Drawing.Point(12, 9);
+            this.lblKorIme.Name = "lblKorIme";
+            this.lblKorIme.Size = new System.Drawing.Size(82, 13);
+            this.lblKorIme.TabIndex = 0;
+            this.lblKorIme.Text = "Korisnicko Ime: ";
+            // 
+            // lblLozinka
+            // 
+            this.lblLozinka.AutoSize = true;
+            this.lblLozinka.Location = new System.Drawing.Point(12, 42);
+            this.lblLozinka.Name = "lblLozinka";
+            this.lblLozinka.Size = new System.Drawing.Size(47, 13);
+            this.lblLozinka.TabIndex = 1;
+            this.lblLozinka.Text = "Lozinka:";
+            // 
+            // txtKorIme
+            // 
+            this.txtKorIme.Location = new System.Drawing.Point(95, 9);
+            this.txtKorIme.Name = "txtKorIme";
+            this.txtKorIme.Size = new System.Drawing.Size(100, 20);
+            this.txtKorIme.TabIndex = 2;
+            // 
+            // txtLozinka
+            // 
+            this.txtLozinka.Location = new System.Drawing.Point(95, 42);
+            this.txtLozinka.Name = "txtLozinka";
+            this.txtLozinka.Size = new System.Drawing.Size(100, 20);
+            this.txtLozinka.TabIndex = 3;
+            // 
+            // btnPrijava
+            // 
+            this.btnPrijava.Location = new System.Drawing.Point(156, 68);
+            this.btnPrijava.Name = "btnPrijava";
+            this.btnPrijava.Size = new System.Drawing.Size(75, 23);
+            this.btnPrijava.TabIndex = 4;
+            this.btnPrijava.Text = "Prijava";
+            this.btnPrijava.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(767, 415);
+            this.ClientSize = new System.Drawing.Size(233, 104);
+            this.Controls.Add(this.btnPrijava);
+            this.Controls.Add(this.txtLozinka);
+            this.Controls.Add(this.txtKorIme);
+            this.Controls.Add(this.lblLozinka);
+            this.Controls.Add(this.lblKorIme);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblKorIme;
+        private System.Windows.Forms.Label lblLozinka;
+        private System.Windows.Forms.TextBox txtKorIme;
+        private System.Windows.Forms.TextBox txtLozinka;
+        private System.Windows.Forms.Button btnPrijava;
     }
 }
 
