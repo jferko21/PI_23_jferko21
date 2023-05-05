f8efa5e20cc00898e86931aac956d5842f597b8f
jferko21 <126473832+jferko21@users.noreply.github.com>
2023-05-05 10:32:51 +0200

    Dovršenje vježbe

--- EvaluationManager/Form1.cs
+++ EvaluationManager/Form1.cs
@@ -10,12 +10,36 @@ using System.Windows.Forms;
 
 namespace EvaluationManager {
     public partial class Form1 : Form {
+        string username = "nastavnik";
+        string password = "test";
+
         public Form1() {
             InitializeComponent();
         }
 
         private void btnPrijava_Click(object sender, EventArgs e) {
+            if (txtKorIme.Text == "")
+            {
+                MessageBox.Show("Korisničko ime nije uneseno!", "Problem",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txtLozinka.Text == "")
+            {
+                MessageBox.Show("Lozinka nije uneseno!", "Problem",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else {
+                if (txtKorIme.Text == username && txtLozinka.Text == password)
+                {
+                    FrmStudents frmStudents = new FrmStudents();
+                    frmStudents.ShowDialog();
+                }
+                else {
+                    MessageBox.Show("Krivi podaci!", "Problem",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
 
+                }
+            }
         }
     }
 }
