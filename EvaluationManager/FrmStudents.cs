b552de478077316d1688b8b21f46bf8dbecd7193
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-05-08 12:12:14 +0200

    Popravljen poredak prikaza na gridu

--- EvaluationManager/FrmStudents.cs
+++ EvaluationManager/FrmStudents.cs
@@ -19,10 +19,13 @@ namespace EvaluationManager
         }
 
         private void ShowStudents() {
-            List<Student> students = new List<Student>();
             StudentRepository repo = new StudentRepository();
-
             dgvStudents.DataSource = repo.GetStudents();
+
+            dgvStudents.Columns["Id"].DisplayIndex = 0;
+            dgvStudents.Columns["FirstName"].DisplayIndex = 1;
+            dgvStudents.Columns["LastName"].DisplayIndex = 2;
+            dgvStudents.Columns["Grade"].DisplayIndex = 3;
 
         }
 
